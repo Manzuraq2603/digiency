a58e49e36fe8a9f081c0a7728292409a157af648
Manzuraq2603 <manzuraq2001@gmail.com>
2025-07-15 21:16:08 +0500

    add about page with responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,37 +8,42 @@ function About() {
     return (
         <section className="py-30 bg-white">
             <div className="max-w-[1250px] mx-auto px-10 ">
-                <div className="flex justify-between">
+                <div className="flex justify-between md:flex-row flex-col ">
                     <img
-                        className="mt-auto"
+                        className="mt-auto lg:w-[684px] md:w-[372px] lg:h-[352px] md:h-[192px] w-full md:mb-0 mb-[50px]"
                         src={aboutImg}
                         alt="image"
                         width={684}
                         height={352}
                     />
-                    <div className="max-w-[570px] w-full">
-                        <p className="font-normal text-[#7B7B7B] text-base leading-[151%] mb-[25px]">
+                    <div className="lg:max-w-[570px] md:max-w-83 w-full ">
+                        <p className="font-normal text-[#7B7B7B] text-base leading-[151%] mb-[25px] md:text-start text-center   ">
                             About Us
                         </p>
-                        <h2 className="font-semibold text-[39px] leading-[124%] mb-[18px] text-[#373737]">
+                        <h2 className="md:font-semibold font-medium lg:text-[39px] md:text-[31px] text-[25px] md:text-start text-center leading-[124%] mb-[18px] text-[#373737]">
                             We Are{" "}
                             <span className="text-[#FF5300]">
                                 Making Ideas Better
                             </span>{" "}
                             For Everyone
                         </h2>
-                        <p className="mb-[25px] font-normal text-[#7B7B7B] text-base leading-[151%] ">
+                        <p className="mb-[25px] font-normal text-[#7B7B7B] text-base md:text-start text-center leading-[151%] ">
                             Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Nascetur imperdiet at a augue ullamcorper. Leo
-                            faucibus feugiat rutrum aenean. Nisi, lectus aliquet
-                            aliquam consectetur augue praesent. Lorem vulputate
-                            orci eget mi, sed pulvinar.
+                            elit. Nascetur imperdiet at a augue ullamcorper.{" "}
+                            <span className="lg:flex hidden">
+                                {" "}
+                                Leo faucibus feugiat rutrum aenean. Nisi, lectus
+                                aliquet aliquam consectetur augue praesent.
+                                Lorem vulputate orci eget mi, sed pulvinar.
+                            </span>
                         </p>
-                        <p className="mb-[25px] font-normal text-[#7B7B7B] text-base leading-[151%] ">
+                        <p className="mb-[25px] font-normal text-[#7B7B7B] text-base leading-[151%] md:text-start text-center ">
                             Ornare etiam erat volutpat tempor fringilla mi. Elit
                             a blandit faucibus est, dui interdum ut amet.
-                            Adipiscing feugiat vel at posuere in. Pellentesque
-                            volutpat vestibulum.
+                            <span className="lg:flex hidden">
+                                Adipiscing feugiat vel at posuere in.
+                                Pellentesque volutpat vestibulum.
+                            </span>
                         </p>
                         <div className="flex items-center md:justify-start justify-center">
                             <button className="font-medium text-xl leading-[124%] rounded-[5px]  pl-[19px] pr-[22px] pt-[13px] pb-4 text-white bg-[#FF5300]">
@@ -47,8 +52,8 @@ function About() {
                         </div>
                     </div>
                 </div>
-                <ul className="mt-15 flex justify-between items-center ">
-                    <li className="shadow-[0px_1px_10px_0px_#0000001A] max-w-[270px] h-50 w-full flex flex-col items-center pt-[25px] pb-[21px] gap-9 text-center">
+                <ul className="mt-15 md:flex-row flex flex-col gap-[30px] items-center ">
+                    <li className="shadow-[0px_1px_10px_0px_#0000001A] md:max-w-[270px] h-50  w-full flex flex-col items-center pt-[25px] pb-[21px] gap-9 text-center">
                         <div className=" shadow-[4px_5px_15px_0px_#FF530066] w-[70px] h-[70px] bg-[#FF5300] flex justify-center items-center rounded-[50%]">
                             <img
                                 src={fileIcon}
@@ -57,12 +62,12 @@ function About() {
                                 height={25}
                             />
                         </div>
-                        <p className=" max-w-[206px] w-full font-normal text-[#7B7B7B] text-base leading-[151%] ">
+                        <p className="max-w-[206px] w-full font-normal text-[#7B7B7B] text-base leading-[151%] ">
                             World leader in consulting and finance
                         </p>
                     </li>
 
-                    <li className="shadow-[0px_1px_10px_0px_#0000001A] max-w-[270px]  h-50 w-full flex flex-col items-center pt-[25px] pb-[21px] gap-9 text-center">
+                    <li className="shadow-[0px_1px_10px_0px_#0000001A] md:max-w-[270px]  h-50 w-full flex flex-col items-center pt-[25px] pb-[21px] gap-9 text-center">
                         <div className="shadow-[4px_5px_15px_0px_#00000066] w-[70px] h-[70px] bg-[#7B7B7B] flex justify-center items-center rounded-[50%]">
                             <img
                                 src={groupIcon}
@@ -76,7 +81,7 @@ function About() {
                         </p>
                     </li>
 
-                    <li className="shadow-[0px_1px_10px_0px_#0000001A] max-w-[270px]  h-50 w-full flex flex-col items-center pt-[25px] pb-[21px] gap-9 text-center">
+                    <li className="shadow-[0px_1px_10px_0px_#0000001A] md:max-w-[270px]  h-50 w-full flex flex-col items-center pt-[25px] pb-[21px] gap-9 text-center">
                         <div className="shadow-[4px_5px_15px_0px_#00C2FF66] w-[70px] h-[70px] bg-[#00ACFF] flex justify-center items-center rounded-[50%]">
                             <img
                                 src={settings}
@@ -90,7 +95,7 @@ function About() {
                         </p>
                     </li>
 
-                    <li className="shadow-[0px_1px_10px_0px_#0000001A] max-w-[270px]  h-50 w-full flex flex-col items-center pt-[25px] pb-[21px] gap-9 text-center">
+                    <li className="shadow-[0px_1px_10px_0px_#0000001A] md:max-w-[270px]  h-50 w-full flex flex-col items-center pt-[25px] pb-[21px] gap-9 text-center">
                         <div className="shadow-[4px_5px_15px_0px_#00000066] w-[70px] h-[70px] bg-[#373737] flex justify-center items-center rounded-[50%]">
                             <img
                                 src={systems}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,32 +8,36 @@ import skype from "../assets/skype.svg";
 function Hero() {
     return (
         <section className="lg:py-[153px] md:[73px] py-[50px] bg-[#FFF7F4] flex">
-            <div className="max-w-[1250px] mx-auto px-10 md:flex-row flex flex-col-reverse  items-center lg:gap-[35px] md:gap-[19px] ">
-                <div className="lg:max-w-[493px] md:max-w-[349px] w-full">
-                    <h1 className="lg:font-bold font-semibold lg:text-5xl md:text-[39px] text-[31px] md:text-start text-center leading-[124%] text-[#373737]">
-                        We Are The Best{" "}
-                        <span className="text-[#FF5300]">Digital Agency</span>{" "}
-                        for business
-                    </h1>
-                    <p className="font-normal text-base md:text-start text-center leading-[151%] lg:mt-[11px] mt-[18px]">
-                        Amet minim mollit non deserunt ullamco est sit aliqua
-                        dolor do amet sint. Velit officia consequat duis enim
-                        velit mollit. Exercitation veniam consequat sunt nostrud
-                        amet.
-                    </p>
-                    <div className="flex items-center md:justify-start justify-center">
-                        <button className="font-medium text-xl leading-[124%] rounded-[5px] lg:mt-[50px]  mt-[25px] pl-[19px] pr-[22px] pt-[13px] pb-4 text-white bg-[#FF5300]">
-                            Contact Us
-                        </button>
+            <div className="max-w-[1250px] mx-auto px-10">
+                <div className="md:flex-row flex flex-col-reverse  items-center justify-between gap-[35px]">
+                    <div className="lg:max-w-[493px] md:max-w-[349px] w-full">
+                        <h1 className="lg:font-bold font-semibold lg:text-5xl md:text-[39px] text-[31px] md:text-start text-center leading-[124%] text-[#373737]">
+                            We Are The Best{" "}
+                            <span className="text-[#FF5300]">
+                                Digital Agency
+                            </span>{" "}
+                            for business
+                        </h1>
+                        <p className="font-normal text-base md:text-start text-center leading-[151%] lg:mt-[11px] mt-[18px]">
+                            Amet minim mollit non deserunt ullamco est sit
+                            aliqua dolor do amet sint. Velit officia consequat
+                            duis enim velit mollit. Exercitation veniam
+                            consequat sunt nostrud amet.
+                        </p>
+                        <div className="flex items-center md:justify-start justify-center">
+                            <button className="font-medium text-xl leading-[124%] rounded-[5px] lg:mt-[50px]  mt-[25px] pl-[19px] pr-[22px] pt-[13px] pb-4 text-white bg-[#FF5300]">
+                                Contact Us
+                            </button>
+                        </div>
                     </div>
+                    <img
+                        className="lg:w-[639px] lg:h-[429px] md:w-[307px] w-full md:h-[207px] md:mb-0 mb-[50px]"
+                        src={heroImg}
+                        alt="hero-img"
+                        width={639}
+                        height={429}
+                    />
                 </div>
-                <img
-                    className="lg:w-[639px] lg:h-[429px] md:w-[307px] w-full md:h-[207px] md:mb-0 mb-[50px]"
-                    src={heroImg}
-                    alt="hero-img"
-                    width={639}
-                    height={429}
-                />
             </div>
             <div className="  md:flex hidden  flex-col gap-[5px]">
                 <div className=" mt-16  w-10 h-10 bg-amber-600 flex items-center justify-center">
